654511a669e71dd9ac5f104a1ba14dc02aae79a4
RIP Bozza <ripbozza@RIPs-MacBook-Air.local>
2024-10-14 22:57:03 +0530

    prod commit

--- app/models.py
+++ app/models.py
@@ -2,10 +2,37 @@ from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Mapped, mapped_column
+import os
+from dotenv import load_dotenv
+import boto3
 
+
+load_dotenv()
+
+
+def get_ssm_parameter(param_name):
+    """
+    Fetch parameter from AWS SSM Parameter Store.
+    """
+    ssm = boto3.client('ssm', region_name='ap-south-1')
+    parameter = ssm.get_parameter(Name=param_name, WithDecryption=True)
+    return parameter['Parameter']['Value']
+
+
+POSTGRES_USERNAME =os.getenv("POSTGRES_USERNAME")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_NAME=os.getenv("DB_NAME")
+DB_URL =os.getenv("DB_URL")
+
+# if one is not defined automatically take all from aws
+if not  all([POSTGRES_USERNAME ,POSTGRES_PASSWORD ,DB_NAME ,DB_URL]):
+    POSTGRES_USERNAME = get_ssm_parameter("/myapp/POSTGRES_USERNAME")
+    POSTGRES_PASSWORD = get_ssm_parameter("/myapp/POSTGRES_PASSWORD")
+    DB_NAME = get_ssm_parameter("/myapp/DB_NAME")
+    DB_URL = get_ssm_parameter("/myapp/DB_URL")
 
 # PostgreSQL connection URL
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/portfolio_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{DB_URL}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
